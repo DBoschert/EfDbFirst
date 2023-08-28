88ba1e2aa278a94ccd5749b4f8b3e7cbf7deb403
Drew Boschert <drew.r.boschert@gmail.com>
2023-08-28 09:54:40 -0400

    Add project files.

---
+++ EfDbFirst/Program.cs
@@ -0,0 +1,11 @@
+﻿using EfDbFirst.Models;
+
+var _context = new SqlprojectContext();
+
+var users = _context.Users.ToList();
+
+foreach(var user in users)
+{
+    Console.WriteLine($"{user.Firstname} {user.Lastname}");
+}
+    
